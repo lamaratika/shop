c9019828be6fc85f2758e929e1db7c13520d5769
Nicolas <lamaratika1@proton.me>
2025-10-08 05:49:44 +0200

    refactor homepage path function

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,9 +5,14 @@ import pug from "pug";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
+function getHomepagePath(folder: string): string {
+  const viewsPath = normalize(resolve(folder, "../views"));
+  const homePagePath = viewsPath + "/home/index.pug";
+  return homePagePath;
+}
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const viewsPath = normalize(resolve(__dirname, "../views"));
-const homePagePath = viewsPath + "/home/index.pug";
+const homePagePath = getHomepagePath(__dirname);
+
 const homePage = pug.compileFile(homePagePath, { pretty: true })({
   title: "Home",
 });
