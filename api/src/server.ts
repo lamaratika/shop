c51dd7344e5877561a08ac47b2ef0574949c45f9
Nicolas <lamaratika1@proton.me>
2025-10-08 06:50:41 +0200

    serve html with css on home

--- api/src/server.ts
+++ api/src/server.ts
@@ -42,6 +42,61 @@ function errorHandler(res: any) {
   res.end(JSON.stringify({ error: "Internal Server Error" }));
 }
 
+function cssHomePage(): string {
+  return `
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    color: #333;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  header {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 0;
+    text-align: center;
+  }
+  main {
+    padding: 20px;
+    max-width: 800px;
+    margin: 20px auto;
+    background: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+  }
+  h1 {
+    color: #4CAF50;
+  }
+  a {
+    color: #4CAF50;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  footer {
+    text-align: center;
+    padding: 10px 0;
+    background-color: #f4f4f4;
+    color: #777;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.1);
+  }
+  `;
+}
+const cssHomePageStyles = cssHomePage();
+
+console.log("stzles", cssHomePageStyles);
+
 function requestListener(req: any, res: any) {
   switch (req.method) {
     case "GET":
@@ -69,7 +124,7 @@ function requestListener(req: any, res: any) {
       return;
     case "/styles.css":
       res.writeHead(200, { "Content-Type": "text/css" });
-      res.end("body { font-family: Arial, sans-serif; }");
+      res.end(cssHomePageStyles);
       return;
     case "/health":
       healthHandler(res);
